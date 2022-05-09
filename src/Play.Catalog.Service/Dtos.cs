56964a411a9f3a4704e3474d1dad7111baa2b553
iqfy-ahmed <ahmed@iqualify.com>
2022-05-10 09:24:55 +1200

    add post, put and delete

--- src/Play.Catalog.Service/Dtos.cs
+++ src/Play.Catalog.Service/Dtos.cs
@@ -4,7 +4,7 @@ namespace Play.Catalog.Service.Dtos
 {
   public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
 
-  public record createItemDto(string Name, string Description, decimal Price);
+  public record CreateItemDto(string Name, string Description, decimal Price);
 
   public record UpdateItemDto(string Name, string Description, decimal Price);
 }
