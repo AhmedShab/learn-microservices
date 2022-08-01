0270b6fcc12ae8fe81db8a65ea0ccc01a1fa63ab
iqfy-ahmed <hello@ahmedshaaban.tech>
2022-08-01 13:30:18 +1200

    set up inventory api

--- Play.Inventory/src/Play.Inventory.Service/Controllers/ItemsController.cs
+++ Play.Inventory/src/Play.Inventory.Service/Controllers/ItemsController.cs
@@ -4,62 +4,61 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Play.Common;
+using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Dto;
 using Play.Inventory.Service.Entities;
 
-namespace Play.Inventory.Service.Controllers
-{
-		[ApiController]
-		[Route("items")]
-		public class ItemsController : ControllerBase
-		{
-				private readonly IRepository<InventoryItem> itemsRepository;
+namespace Play.Inventory.Service.Controllers {
+  [ApiController]
+  [Route("items")]
+  public class ItemsController : ControllerBase {
+    private readonly IRepository<InventoryItem> itemsRepository;
+    private readonly CatalogClient catalogClient;
 
-				public ItemsController(IRepository<InventoryItem> itemsRepository)
-				{
-						this.itemsRepository = itemsRepository;
-				}
+    public ItemsController(IRepository<InventoryItem> itemsRepository, CatalogClient catalogClient) {
+      this.itemsRepository = itemsRepository;
+      this.catalogClient = catalogClient;
+    }
 
-				[HttpGet]
-				public async Task<ActionResult<IEnumerable<InventoryItemDto>>> GetAsync(Guid userId)
-				{
-						if (userId == Guid.Empty)
-						{
-								return BadRequest();
-						}
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<InventoryItemDto>>> GetAsync(Guid userId) {
+      if (userId == Guid.Empty) {
+        return BadRequest();
+      }
 
-						var items = (await itemsRepository.GetAllAsync(item => item.UserId == userId))
-												.Select(item => item.AsDto());
+      var catalogItems = await catalogClient.GetCatalogItemsAsync();
+      var inventoryItemEntities = await itemsRepository.GetAllAsync(item => item.UserId == userId);
 
-						return Ok(items);
-				}
+      var inventoryItemDtos = inventoryItemEntities.Select(inventoryItem => {
+        var catalogItem = catalogItems.Single(catalogItem => catalogItem.Id == inventoryItem.CatalogItemId);
 
-				[HttpPost]
-				public async Task<ActionResult> PostAsync(GrandItemsDto grandItemsDto)
-				{
-						var inventoryItem = await itemsRepository.GetAsync(
-							item => item.UserId == grandItemsDto.UserId && item.CatalogItemId == grandItemsDto.CatalogItemId
-						);
+        return inventoryItem.AsDto(catalogItem.Name, catalogItem.Description);
+      });
 
-						if (inventoryItem == null)
-						{
-								inventoryItem = new InventoryItem
-								{
-										CatalogItemId = grandItemsDto.CatalogItemId,
-										UserId = grandItemsDto.UserId,
-										Quantity = grandItemsDto.Quality,
-										AcquiredDate = DateTimeOffset.UtcNow
-								};
+      return Ok(inventoryItemDtos);
+    }
 
-								await itemsRepository.CreateAsync(inventoryItem);
-						}
-						else
-						{
-								inventoryItem.Quantity += grandItemsDto.Quality;
-								await itemsRepository.UpdateAsync(inventoryItem);
-						}
+    [HttpPost]
+    public async Task<ActionResult> PostAsync(GrandItemsDto grandItemsDto) {
+      var inventoryItem = await itemsRepository.GetAsync(
+        item => item.UserId == grandItemsDto.UserId && item.CatalogItemId == grandItemsDto.CatalogItemId
+      );
 
-						return Ok();
-				}
-		}
+      if (inventoryItem == null) {
+        inventoryItem = new InventoryItem {
+          CatalogItemId = grandItemsDto.CatalogItemId,
+          UserId = grandItemsDto.UserId,
+          Quantity = grandItemsDto.Quality,
+          AcquiredDate = DateTimeOffset.UtcNow
+        };
+
+        await itemsRepository.CreateAsync(inventoryItem);
+      } else {
+        inventoryItem.Quantity += grandItemsDto.Quality;
+        await itemsRepository.UpdateAsync(inventoryItem);
+      }
+
+      return Ok();
+    }
+  }
 }
--- Play.Inventory/src/Play.Inventory.Service/Dtos.cs
+++ Play.Inventory/src/Play.Inventory.Service/Dtos.cs
@@ -4,7 +4,7 @@ namespace Play.Inventory.Service.Dto
 {
     public record GrandItemsDto(Guid UserId, Guid CatalogItemId, int Quality);
 
-    public record InventoryItemDto(Guid CatalogItemId, int Quantity, DateTimeOffset AcquiredDate);
+    public record InventoryItemDto(Guid CatalogItemId, string Name, string Description, int Quantity, DateTimeOffset AcquiredDate);
 
-    public record CatalogItemDto(Guid Id, string Description);
+    public record CatalogItemDto(Guid Id, string Name, string Description);
 }
