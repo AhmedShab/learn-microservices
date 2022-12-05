58d28f8d3568ed3ded72c7f3d0a30f1f433c4556
AhmedShab <hello@ahmedshaaban.tech>
2022-12-05 16:48:08 +1300

    create catalog item create class

--- Play.Inventory/src/Play.Inventory.Service/Entities/CatalogItem.cs
+++ Play.Inventory/src/Play.Inventory.Service/Entities/CatalogItem.cs
@@ -3,12 +3,12 @@ using Play.Common;
 
 namespace Play.Inventory.Service.Entities
 {
-    public class CatalogItem : IEntity
-    {
-        public Guid Id { get; set; }
+  public class CatalogItem : IEntity
+  {
+    public Guid Id { get; set; }
 
-        public Guid Name { get; set; }
+    public string Name { get; set; }
 
-        public Guid Description { get; set; }
-    }
+    public string Description { get; set; }
+  }
 }
